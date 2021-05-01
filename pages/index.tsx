487380f675f0a4eaf1f8a8d45c44144ebad56e7d
Yuichi Yogo <yuichkun@gmail.com>
2021-05-01 11:26:42 +0900

    show ContributionChart

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,26 @@
 import { NextPage } from "next";
+import { ContributionChart } from '../components/ContributionChart'
+import { retrieveContributionData } from "../modules/github";
 
-const IndexPage: NextPage = () => {
+type Props = {
+  contributionDays: Externals.Github.ContributionDay[]
+}
+
+const IndexPage: NextPage<Props> = ({ contributionDays }) => {
   return (
-    <div>hello</div>
+    <ContributionChart contributionDays={contributionDays}/>
   )
+}
+
+IndexPage.getInitialProps = async () => {
+  const { data: { user: { contributionsCollection: { contributionCalendar: { totalContributions, weeks }} }} } = await retrieveContributionData('yuichkun')
+  console.log(totalContributions)
+  const contributionDays = weeks.reduce(((prev, cur) => {
+    return prev.concat(cur.contributionDays)
+  }),[] as Externals.Github.ContributionDay[])
+  return {
+    contributionDays
+  }
 }
 
 export default IndexPage
